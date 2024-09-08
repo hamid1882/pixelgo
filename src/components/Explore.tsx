f8c84c8b5c0a9c04820bbed6f0c1f6c791c76373
hamid1882 <hussainhafeez1100@gmail.com>
2024-09-09 00:46:18 +0530

    fix: typography explore section

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -7,11 +7,11 @@ function Explore() {
   return (
     <div className="container mx-auto px-[20px] lg:px-0 py-[20px] lg:py-[36px]">
       <div className="flex items-center w-full">
-        <div className="w-full text-left lg:text-center">
-          <h2 className="text-[23px] font-medium">
+        <div className="w-full text-left lg:text-center space-y-[8px]">
+          <h2 className="text-[23px] font-medium leading-[25px] lg:leading-[36px]">
             Targeted Lead Generation Strategies
           </h2>
-          <p>
+          <p className="leading-[22px]">
             Capture and convert high-quality leads for your business growth.
           </p>
         </div>
